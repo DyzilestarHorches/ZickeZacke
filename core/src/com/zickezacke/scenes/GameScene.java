077f4a6b25a5cd2419d0f970ef476514b5e5ccf1
JimHoth <71512142+JimHoth@users.noreply.github.com>
2023-01-03 21:25:22 +0100

    jimhoth

--- core/src/com/zickezacke/scenes/GameScene.java
+++ core/src/com/zickezacke/scenes/GameScene.java
@@ -6,10 +6,27 @@ import com.zickezacke.gameObjectStore.GameScene.OctTiles;
 import com.zickezacke.nclib.game.screens.helpers.GameWorld;
 import com.zickezacke.gameObjectStore.testObject;
 
+
+import java.io.Console;
+
+
 public class GameScene extends GameWorld {
 
-    public GameScene(boolean has3DCamera, boolean has2DCamera){
+    private  int currentFrame = 0;
+    // determines if the game is in player's turn
+    private boolean isRunning;
+    // determines if one player have win
+    private  boolean isEnd;
+    private int currentPlayer;
+    private int totalPlayer;
+
+    public GameScene(boolean has3DCamera, boolean has2DCamera)
+    {
         super(has3DCamera, has2DCamera);
+        isRunning = true;
+        isEnd = false;
+        currentPlayer = 0;
+        totalPlayer = 4;
     }
 
     public void Begin(){
@@ -23,6 +40,32 @@ public class GameScene extends GameWorld {
         //Gdx.app.log("Number of GO3D", Integer.toString(gameObjects3D.size()));
         //gameObjects.add(new backGround(102));
         //gameObjects.add(new backGround3D(103));
+    }
+
+
+    @Override
+    public void Update() {
+        super.Update();
+        currentFrame++;
+
+        if (isRunning) {
+
+        } else {
+            startNextPlayer();
+        }
+
+        if (isEnd) {
+            System.out.println("Winner winner chicken dinner");
+        }
+    }
+
+    /**
+     * ends current player turn and starts the next player turns
+     */
+    public void startNextPlayer() {
+        currentPlayer = (currentPlayer + 1) % 4;
+        System.out.println("Next player! " + currentPlayer);
+        isRunning = true;
     }
 
 }
