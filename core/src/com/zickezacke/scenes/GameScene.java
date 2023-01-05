74f6781d0711ad321fb896acbc92809fb05094f0
CheeseHu <94207173+CheeseHu@users.noreply.github.com>
2023-01-05 17:47:36 +0100

    UPDATE

--- core/src/com/zickezacke/scenes/GameScene.java
+++ core/src/com/zickezacke/scenes/GameScene.java
@@ -1,5 +1,6 @@
 package com.zickezacke.scenes;
 
+
 import com.zickezacke.gameObjectStore.GameScene.Ground;
 import com.zickezacke.gameObjectStore.GameScene.Ground_1;
 import com.zickezacke.gameObjectStore.GameScene.Obj;
@@ -16,40 +17,11 @@ public class GameScene extends GameWorld {
         super(has3DCamera, has2DCamera);
     }
     public void Begin(){
-//        float a = 1.5f;
-        float a = 1.2f;
 
-        //gameObjects3D.add(new testObject(100));
-        //gameObjects3D.add(new Ground(101));
-        gameObjects3D.add(new Ground_1(102));
-
-        /*for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                gameObjects3D.add(new Obj(104,(float)(i*a),(float)(-j*a)));
-                gameObjects3D.add(new Obj(105,(float)(i*a),(float)(-j*a)));
-                gameObjects3D.add(new Obj(106,(float)(i*a),(float)(-j*a)));
-                gameObjects3D.add(new Obj(107,(float)(i*a),(float)(-j*a)));
-            }
-        }
-        for(int i = 0; i < 4; i++){
-            for(int j = 0; j < 4; j++){
-                gameObjects3D.add(new card(104,(float)(i*a),(float)(-j*a)));
-                gameObjects3D.add(new card(105,(float)(i*a),(float)(-j*a)));
-                gameObjects3D.add(new card(106,(float)(i*a),(float)(-j*a)));
-                gameObjects3D.add(new card(107,(float)(i*a),(float)(-j*a)));
-            }
-        }*/
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 6; j++){
-                gameObjects3D.add(new card(104,(float)(i*a),(float)(-j*a)));
-                gameObjects3D.add(new card(105,(float)(-i*a),(float)(-j*a)));
-                gameObjects3D.add(new card(106,(float)(-i*a),(float)(j*a)));
-                gameObjects3D.add(new card(107,(float)(i*a),(float)(j*a)));
-            }
-        }
+        gameObjects3D.add(new Ground(101));
 
 
-
+        gameObjects3D.add(new Ground(101));
 
     }
 }
